0eb510080be99eea9afd0560bece8c3c9fb0263e
mirekdusin <32246499+mirekdusin@users.noreply.github.com>
2021-03-22 13:07:20 +0100

    Create frequencies.php

---
+++ frequencies.php
@@ -0,0 +1,16 @@
+<?php
+
+$file = fopen("binary.csv", "r");
+
+$all_comb = array();
+$line = fgets($file);
+
+while (!feof($file)) {
+    $line = fgets($file);
+    $line = preg_replace("/\r|\n/", "", $line);
+    array_push($all_comb, $line);
+}
+
+$all_keywords = array_count_values($all_comb);
+arsort($all_keywords);
+print_r($all_keywords);
